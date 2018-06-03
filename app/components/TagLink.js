c3696b05f80930f295720cd1d1ec8fd7051fcf3c
pc035860 <pc035860@gmail.com>
2018-06-03 15:16:24 +0800

    Fix display for YouTube design change (#6)
    close #6

--- app/components/TagLink.js
+++ app/components/TagLink.js
@@ -10,22 +10,24 @@ import { toTag } from '_util/ytTime';
 import noop from '_util/noop';
 import is2017NewDesign from '_util/is2017NewDesign';
 
-const CLASS_NAME_BASE = is2017NewDesign() ? ['yt-simple-endpoint', 'yt-formatted-string'] : '';
+const CLASS_NAME_BASE = is2017NewDesign()
+  ? ['yt-simple-endpoint', 'style-scope', 'yt-formatted-string']
+  : '';
 
 const TagLink = ({ videoId, seconds, onClick, className }) => {
   const aElm = (
-    <a href={`/watch?v=${videoId}&t=${seconds >>> 0}s`}
+    <a
+      href={`/watch?v=${videoId}&t=${seconds >>> 0}s`}
       styleName="component"
       className={classNames(CLASS_NAME_BASE, className)}
-      onClick={onClick}>{toTag(seconds)}</a>
+      onClick={onClick}
+    >
+      {toTag(seconds)}
+    </a>
   );
 
   if (is2017NewDesign()) {
-    return (
-      <yt-formatted-string class="ytd-comment-renderer">
-        {aElm}
-      </yt-formatted-string>
-    );
+    return <yt-formatted-string class="ytd-comment-renderer">{aElm}</yt-formatted-string>;
   }
   return aElm;
 };
--- app/components/YTButton.js
+++ app/components/YTButton.js
@@ -5,13 +5,15 @@ import { compose, setDisplayName } from 'recompose';
 
 import is2017NewDesign from '_util/is2017NewDesign';
 
+import styles from './YTButton.scss';
+
 const CLASS_NAME_BASE = is2017NewDesign() ? 'ytd-button-renderer' : '';
 const PROPS = is2017NewDesign() ? { is: 'paper-icon-button-light' } : {};
 
 const YTButton = ({ is, className, ...restProps }) => (
   <button
     is={is}
-    className={classNames(CLASS_NAME_BASE, className)}
+    class={classNames(CLASS_NAME_BASE, styles.component, className)}
     {...PROPS}
     {...restProps}
   />
