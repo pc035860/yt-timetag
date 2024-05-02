e1daf59a9c3a792fc1bb737c005c8f1c80bf394e
pc035860 <pc035860@gmail.com>
2024-05-02 18:16:50 +0800

    Migrate manifest.json to v3

--- app/background.js
+++ app/background.js
@@ -1,24 +1,29 @@
 import urlParser from 'js-video-url-parser';
 import {
   MSG_CHECK_LOADED_REQUEST,
-  MSG_CHECK_LOADED_RESPONSE
+  MSG_CHECK_LOADED_RESPONSE,
 } from '_constants/Messages';
 
-chrome.browserAction.onClicked.addListener(tab => {
+chrome.action.onClicked.addListener((tab) => {
   const parsed = urlParser.parse(tab.url);
   if (parsed && parsed.provider === 'youtube' && parsed.mediaType === 'video') {
-    chrome.tabs.sendMessage(tab.id, MSG_CHECK_LOADED_REQUEST, res => {
+    chrome.tabs.sendMessage(tab.id, MSG_CHECK_LOADED_REQUEST, (res) => {
+      // check lastError
+      if (chrome.runtime.lastError) {
+        console.debug('chrome.runtime.lastError', chrome.runtime.lastError);
+      }
+
       if (res === MSG_CHECK_LOADED_RESPONSE) {
         return;
       }
 
-      chrome.tabs.executeScript(tab.id, {
-        file: 'contentscript.js',
-        runAt: 'document_end'
+      chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        files: ['contentscript.js'],
       });
-      chrome.tabs.insertCSS(tab.id, {
-        file: 'contentscript.css',
-        runAt: 'document_end'
+      chrome.scripting.insertCSS({
+        target: { tabId: tab.id },
+        files: ['contentscript.css'],
       });
     });
   }
